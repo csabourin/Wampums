7c04f732cf9dcf29710174fcc1e6cd003a0261ae
Christian Sabourin <info@christiansabourin.com>
2025-12-18 15:53:49 +0000

    Clean up old migrations

--- migrations/add_inscription_date_to_participant_organizations.sql
+++
@@ -1,7 +0,0 @@
--- Add inscription_date column to participant_organizations table
-ALTER TABLE public.participant_organizations
-ADD COLUMN IF NOT EXISTS inscription_date date;
-
--- Create index for better performance when querying by inscription_date
-CREATE INDEX IF NOT EXISTS idx_participant_organizations_inscription_date
-ON public.participant_organizations(inscription_date);
