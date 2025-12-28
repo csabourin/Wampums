1c39495d0835cb40781c3bd987006e49ada966e0
Christian Sabourin <info@christiansabourin.com>
2025-12-28 16:25:34 +0000

    Bump

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Version should match package.json and config.js
-const APP_VERSION = "2.7.0";
+const APP_VERSION = "2.7.1";
 const CACHE_NAME = `wampums-app-v${APP_VERSION}`;
 const STATIC_CACHE_NAME = `wampums-static-v${APP_VERSION}`;
 const API_CACHE_NAME = `wampums-api-v${APP_VERSION}`;
